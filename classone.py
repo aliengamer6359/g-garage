19f0ff95481acd79f1edb7c938929823f1292c23
juan martinez <ju4nf3martinez@gmail.com>
2025-06-04 17:04:24 -0500

    :tada:primera clase:tada:

---
+++ classone.py
@@ -0,0 +1,28 @@
+a = 5
+b = 3 
+c = a + b
+print("c es igual a", a + b) 
+
+print ("cuanto es 8 - 3?") 
+
+respuesta = int(input("¿Cuánto es c - b? "))
+if respuesta == c - b:
+    print("¡Correcto!")
+elif respuesta < c - b:
+    print("Demasiado bajo.")
+else:
+    print("Demasiado alto.")
+
+print("segundo ejercicio")
+
+print ("hola mi nombre es juan")
+edad = 15 
+peso = 56
+estatura = 160
+print("Tengo", edad, "años.")
+
+pregunta2 = int(input("cual es tu edad?"))
+if pregunta2 < 18:
+    print("eres menor de edad")
+else:
+    print("eres mayor de edad")
