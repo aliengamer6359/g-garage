0f67e09292297ee7c5eabade3d719329ce726f44
juan martinez <ju4nf3martinez@gmail.com>
2025-06-04 17:19:20 -0500

    :tada:for and while:tada:

--- classone.py
+++ classone.py
@@ -1,3 +1,4 @@
+##operacion con a b c
 a = 5
 b = 3 
 c = a + b
@@ -15,14 +16,32 @@ else:
 
 print("segundo ejercicio")
 
+##informacion
+
 print ("hola mi nombre es juan")
 edad = 15 
 peso = 56
 estatura = 160
 print("Tengo", edad, "años.")
 
+## edad
+
 pregunta2 = int(input("cual es tu edad?"))
 if pregunta2 < 18:
     print("eres menor de edad")
 else:
     print("eres mayor de edad")
+
+##for y while 
+
+# Ejemplo de bucle for
+print("Contando del 1 al 5 con for:")
+for i in range(1, 6):
+    print(i)
+
+# Ejemplo de bucle while
+print("Contando del 5 al 1 con while:")
+contador = 5
+while contador > 0:
+    print(contador)
+    contador -= 1
